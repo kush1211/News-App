efc29ae516b07acddae124a826b2f4d507a1176d
kush1211 <kushmodi411@gmail.com>
2023-10-31 17:56:33 +0530

    updated articles

--- src/classCompo/News.js
+++ src/classCompo/News.js
@@ -158,7 +158,7 @@ const News = (props) => {
             {
             loading && <Spinner />}
             <InfiniteScroll
-                dataLength={articles.length}
+                dataLength={articles && articles.length}
                 next={fetchMoreData}
                 hasMore={articles.length !== totalResults}
                 loader={<Spinner />}
